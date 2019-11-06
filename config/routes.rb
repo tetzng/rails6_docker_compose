dbc3002124d4a35e752130de629394de89876110
tetzng <ffffffffffffunk@gmail.com>
2019-11-06 21:57:13 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'works#index'
   get 'works/index'
   get 'lists/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
